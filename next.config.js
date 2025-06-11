e5652d346593c3ce67ad2ac992fdcb26519eaf7d
Omri Harel <omriharel@gmail.com>
2025-06-11 20:41:46 +0300

    Content-Disposition fix

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/images/dishes/:filename",
+        destination:
+          "/_next/image?url=%2Fimages%2Fdishes%2F:filename&w=1080&q=75",
+      },
+      {
         source: '/ingest/static/:path*',
         destination: 'https://eu-assets.i.posthog.com/static/:path*',
       },
@@ -19,6 +24,20 @@ const nextConfig = {
       {
         source: '/ingest/decide',
         destination: 'https://eu.i.posthog.com/decide',
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/images/dishes/:filename",
+        headers: [
+          {
+            key: "Content-Disposition",
+            value: "attachment; filename=:filename",
+          },
+        ],
       },
     ];
   },
